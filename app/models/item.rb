ff6b782b417905f7036c66edd13ec6ca78dba5bc
Michael Luna <michaelluna244@gmail.com>
2025-01-11 21:02:15 -0700

    normalize the name column to downcase

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,8 @@
 class Item < ApplicationRecord
   belongs_to :grocery_list
 
+  normalizes :name, with: ->(name) { name.downcase }
+
   validates :name, :quantity, presence: true
   validates :name, uniqueness: { case_sensitive: false, scope: :grocery_list }
   validates :quantity, numericality: { greater_than: 0 }
