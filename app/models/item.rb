138d91854b59ed2bdb58b4c467d081f2863b317a
Michael Luna <michaelluna244@gmail.com>
2025-01-11 20:54:58 -0700

    fix the scope on the items name validation

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,6 @@ class Item < ApplicationRecord
   belongs_to :grocery_list
 
   validates :name, :quantity, presence: true
-  validates :name, uniqueness: { case_sensitive: false, scope: grocery_list }
+  validates :name, uniqueness: { case_sensitive: false, scope: :grocery_list }
   validates :quantity, numericality: { greater_than: 0 }
 end
