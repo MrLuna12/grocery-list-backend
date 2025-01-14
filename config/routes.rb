797eab4203e5a15a655e3e383cd33a0d5171276d
Michael Luna <michaelluna244@gmail.com>
2025-01-13 19:34:31 -0700

    set the grocery_list index as the apps root

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
-  root "home#index"
+  root "grocery_lists#index"
 
   authenticate :user do
     resources :grocery_lists do
