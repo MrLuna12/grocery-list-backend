fe590d78dcff6172fd40b1524ef83bce63a2ab73
Michael Luna <michaelluna244@gmail.com>
2025-01-17 22:40:41 -0700

    add the checked_at to the strong params

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -32,7 +32,7 @@ class ItemsController < ApplicationController
   end
 
   def item_params
-    params.require(:item).permit(:name, :quantity, :grocery_list_id)
+    params.require(:item).permit(:name, :quantity, :grocery_list_id, :checked_at)
   end
 
   private
