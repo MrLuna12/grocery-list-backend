8ba0941a3103e31e0fb959567fa5024a970f5260
Michael Luna <michaelluna244@gmail.com>
2025-01-17 22:37:46 -0700

    scope list name uniqueness to the user

--- app/models/grocery_list.rb
+++ app/models/grocery_list.rb
@@ -2,5 +2,5 @@ class GroceryList < ApplicationRecord
   belongs_to :user
   has_many :items, dependent: :destroy
 
-  validates :title, presence: true, uniqueness: { case_sensitive: false }
+  validates :title, presence: true, uniqueness: { case_sensitive: false, scope: :user_id }
 end
