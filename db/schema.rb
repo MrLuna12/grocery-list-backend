519729bac1419031dcfcfb2cefd42be7fffac14f
Michael Luna <michaelluna244@gmail.com>
2025-01-17 18:35:04 -0700

    add a checked_at column to the items table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_01_04_223520) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_15_035303) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -28,6 +28,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_04_223520) do
     t.bigint "grocery_list_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "check_at"
     t.index ["grocery_list_id"], name: "index_items_on_grocery_list_id"
   end
 
