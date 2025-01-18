9c7f96a6e7d0f4cfc1314a154f998d92eb147ee8
Michael Luna <michaelluna244@gmail.com>
2025-01-17 22:40:00 -0700

    titleize the title and normalize the title column

--- app/models/grocery_list.rb
+++ app/models/grocery_list.rb
@@ -2,5 +2,7 @@ class GroceryList < ApplicationRecord
   belongs_to :user
   has_many :items, dependent: :destroy
 
+  normalizes :title, with: ->(title) { title.downcase }
+
   validates :title, presence: true, uniqueness: { case_sensitive: false, scope: :user_id }
 end
