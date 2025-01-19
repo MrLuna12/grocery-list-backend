eeef0a2fc8ff020acfd829745e6aa8e37c717350
Michael Luna <michaelluna244@gmail.com>
2025-01-18 20:16:33 -0700

    use stimulus to update the checked_at column when a user checks it off

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :set_grocery_list
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
     @items = @grocery_list.items.items
@@ -26,6 +27,11 @@ class ItemsController < ApplicationController
   end
 
   def update
+    if @item.update(item_params)
+      redirect_to @grocery_list, notice: "Item was successfully updated."
+    else
+      render :edit, status: :unprocessable_content
+    end
   end
 
   def destroy
@@ -39,5 +45,9 @@ class ItemsController < ApplicationController
 
   def set_grocery_list
     @grocery_list = current_user.grocery_lists.find(params[:grocery_list_id])
+  end
+
+  def set_item
+    @item = @grocery_list.items.find(params[:id])
   end
 end
--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -1,7 +1,25 @@
-import { Controller } from "@hotwired/stimulus"
+import {Controller} from "@hotwired/stimulus"
 
 export default class extends Controller {
-  connect() {
-    console.log("Hello world")
-  }
+    PUT = 'put'
+
+    submit(event) {
+        let isChecked = event.target.checked
+        let checkedAt = isChecked ? new Date() : null
+        let grocery_list_item_path = event.params.url
+        let csrfToken = document.querySelector('meta[name="csrf-token"]').content
+        let params = {
+            item: {checked_at: checkedAt}
+        }
+
+        fetch(grocery_list_item_path, {
+            method: this.PUT,
+            headers: {
+                'Content-Type': 'application/json',
+                'X-CSRF-Token': csrfToken
+            },
+            body: JSON.stringify(params)
+        }).then(response => response)
+            .catch(err => console.log(err))
+    }
 }
