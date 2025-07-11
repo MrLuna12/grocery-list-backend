896927ba64c87c97c8981ad5286d5b736a3b2708
Michael Luna <michaelluna244@gmail.com>
2025-07-10 21:54:14 -0700

    Update item serializer to replace 'completed' attribute with 'checked_at'

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,4 +1,4 @@
 class ItemSerializer < ActiveModel::Serializer
-  attributes :id, :name, :quantity, :completed, :created_at, :updated_at
+  attributes :id, :name, :quantity, :checked_at, :created_at, :updated_at
   belongs_to :grocery_list
 end
